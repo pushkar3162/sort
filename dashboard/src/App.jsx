d9b9c6dce5a2fda7951d515eabc7c4f1ca40604f
RenukaPatil3122 <renukapatil627@gmail.com>
2025-03-01 13:12:36 +0530

    Resolved changes

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,4 +1,6 @@
-import "bootstrap-icons/font/bootstrap-icons.css";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css"; 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
@@ -7,7 +9,11 @@ import AuthForm from "./components/AuthForm";
 import Dashboard from "./pages/Dashboard";
 import EditorDashboard from "./pages/EditorDashboard";
 import ActivityLogs from "./components/ActivityLogs/ActivityLogs"; // ✅ Import ActivityLogs
-import AuthForm from "./components/AuthForm";
+import RoleBasedUI from "./components/RoleBasedUI";
+import MembersList from "./components/MembersList";
+import UploadForm from "./components/UploadForm";
+import Notifications from "./components/Notification";
+import DocumentLogs from "./components/DocumentLogs/DocumentLogs";
 import Doc from "./components/Doc";
 import Version from "./components/version"; // ✅ Import Version
 
@@ -37,7 +43,6 @@ const App = () => {
           path="/members-list"
           element={<MembersList members={members} setMembers={setMembers} />}
         />
-        <Route path="/test/:folderName" element={<Test />} />
       </Routes>
     </Router>
   );
