a4aa5783ada05dc665c47b145434f5612447afc2
RenukaPatil3122 <renukapatil627@gmail.com>
2025-03-24 13:01:58 +0530

    Added working EditorNavbar with theme toggle and logout

--- dashboard/src/components/EditorDashboard/EditorNavbar.jsx
+++ dashboard/src/components/EditorDashboard/EditorNavbar.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Themetoggle from "../Themetoggle";
 import Searchbar from "../Searchbar";
